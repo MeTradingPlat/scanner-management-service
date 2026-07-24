90a66ab04f4f60ae950d3f21ccccee386392bcea
Daniel Contreras <contrerasdaniel142@gmail.com>
2026-07-24 13:42:15 -0500

    fix: Kafka message converter drops LocalDateTime fields (no JavaTimeModule) - same bug as log-service

--- src/main/java/com/metradingplat/scanner_management/infrastructure/configuration/KafkaConsumerConfig.java
+++ src/main/java/com/metradingplat/scanner_management/infrastructure/configuration/KafkaConsumerConfig.java
@@ -1,5 +1,6 @@
 package com.metradingplat.scanner_management.infrastructure.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.support.converter.RecordMessageConverter;
@@ -9,7 +10,7 @@ import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 public class KafkaConsumerConfig {
 
     @Bean
-    public RecordMessageConverter kafkaMessageConverter() {
-        return new StringJsonMessageConverter();
+    public RecordMessageConverter kafkaMessageConverter(ObjectMapper objectMapper) {
+        return new StringJsonMessageConverter(objectMapper);
     }
 }
