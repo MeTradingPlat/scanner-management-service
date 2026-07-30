300fd4f6252f1e9155f0427545d1992b167c0a52
Daniel Contreras <contrerasdaniel142@gmail.com>
2026-07-30 12:12:02 -0500

    test: add temporary diagnostic endpoint to isolate Valor polymorphic loading

---
+++ src/main/java/com/metradingplat/scanner_management/infrastructure/input/controllerGestionarEscaner/controller/TempDiagController.java
@@ -0,0 +1,34 @@
+package com.metradingplat.scanner_management.infrastructure.input.controllerGestionarEscaner.controller;
+
+import com.metradingplat.scanner_management.infrastructure.output.persistence.entities.ValorCondicionalEntity;
+import com.metradingplat.scanner_management.infrastructure.output.persistence.entities.ValorEntity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TempDiagController {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @GetMapping("/debug/valor/{id}")
+    public String debugValor(@PathVariable Long id) {
+        ValorEntity entity = entityManager.find(ValorEntity.class, id);
+        if (entity == null) {
+            return "null entity";
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("class=").append(entity.getClass().getName());
+        sb.append(" enumTipoValor=").append(entity.getEnumTipoValor());
+        if (entity instanceof ValorCondicionalEntity vc) {
+            sb.append(" enumCondicional=").append(vc.getEnumCondicional());
+            sb.append(" isInteger=").append(vc.getIsInteger());
+            sb.append(" valor1=").append(vc.getValor1());
+            sb.append(" valor2=").append(vc.getValor2());
+        }
+        return sb.toString();
+    }
+}
